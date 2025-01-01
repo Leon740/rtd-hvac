2336f8bc4646995c2dc29b95064bcabb9529fa2b
Leon740 <dobrinovleonid@gmail.com>
2024-12-31 22:13:27 -0500

    build

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,7 @@ export default {
     extend: {
       container: {
         center: true,
-        padding: '16px'
+        padding: '32px'
       },
       fontFamily: {
         'inter-regular': 'InterTight_Regular',
